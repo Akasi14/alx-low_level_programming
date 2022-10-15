38185afc3d84e1610730ccb9b812f87ef57dcbb4
Akasi14 <akasiessuman@gmail.com>
2022-10-14 22:17:06 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,14 @@ int main(void)
 		{
 			putchar('0' + i / 10);
 			putchar('0' + i % 10);
+
 			putchar(' ');
+
 			putchar('0' + j / 10);
 			putchar('0' + j % 10);
+
 			if (i == 98 && j == 99)
-			break;
+				break;
 			putchar(',');
 			putchar(' ');
 		}
